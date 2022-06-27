06fdc7cab6ad9c9c216642ffda59c2320be32a9b
NataliaNNH <91895418+NataliaNNH@users.noreply.github.com>
2022-06-27 23:22:25 +0200

    Update dbconnect.php

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Polaczenie sie z baza danych */
-$conn =mysqli_connect("b67d3db54acc4f","78247360","","blog");
+$conn =mysqli_connect("b67d3db54acc4f","78247360","","@eu-cdbr-west-02.cleardb.net");
 
 /* sprawdzenie polaczenia*/
 if(mysqli_connect_error()) {
